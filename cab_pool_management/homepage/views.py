188f8f88ca238bf7e6b10083ff01c7cd03fd89c4
Thamizhanbu Elamurugu <30389038+danush02@users.noreply.github.com>
2024-01-04 10:18:39 +0530

    Renamed homepage to index

--- cab_pool_management/homepage/views.py
+++ cab_pool_management/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request,"homepage/homepage.html")
+    return render(request,"homepage/index.html")
