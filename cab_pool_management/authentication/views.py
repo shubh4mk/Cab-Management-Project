8abef86250db1ea57ba1bdd511b2fd3d1508f9e8
Shubham <k.shubh4m@gmail.com>
2024-01-25 23:52:51 +0530

    created user dashboard and integrated with login

--- cab_pool_management/authentication/views.py
+++ cab_pool_management/authentication/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, "homepage/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('login')
+
+
     return render(request, "authentication/signup.html")
 
 def login(request):
+
     return render(request, "authentication/login.html")
 
 def signout(request):
