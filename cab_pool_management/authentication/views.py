86cbadf0b9000cc711292eeb156105f9586f0d11
Shubham <k.shubh4m@gmail.com>
2024-01-25 22:03:56 +0530

    Linked the lgoin pages to the homepage

--- cab_pool_management/authentication/views.py
+++ cab_pool_management/authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "homepage/index.html")
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def login(request):
+    return render(request, "authentication/login.html")
+
+def signout(request):
+    pass
--- cab_pool_management/cab_pool_management/urls.py
+++ cab_pool_management/cab_pool_management/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from homepage.views import homepage,aboutus
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage,name="homepage"),
-    path('aboutUs',aboutus,name="aboutus")
+    path('aboutUs',aboutus,name="aboutus"),
+    path('', include('authentication.urls')),
 ]
