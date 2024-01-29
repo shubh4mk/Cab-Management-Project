c95375354d6c50a77ac0b5241d95fccf03317ef1
Thamizhanbu Elamurugu <30389038+danush02@users.noreply.github.com>
2024-01-30 01:33:21 +0530

    Added Password Authentication

--- cab_pool_management/authentication/views.py
+++ cab_pool_management/authentication/views.py
@@ -12,7 +12,6 @@ def home(request):
 
 
 def signup(request):
-
     if request.method == "POST":
         username = request.POST['username']
         fname = request.POST['fname']
@@ -20,33 +19,32 @@ def signup(request):
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-
-        myuser.save()
-
-        messages.success(
-            request, "Your account has been successfully created.")
-
-        return redirect('signin')
+        if not User.objects.filter(username=username).exists():
+            if pass1==pass2:
+                myuser = User.objects.create_user(username, email, pass1)
+                myuser.first_name = fname
+                myuser.last_name = lname
+                myuser.save()
+                return render(request,"authentication/signin.html",{'success_message': "User got Created"})
+            else:
+                return render(request,"authentication/signup.html",{'password_error_message': "Check your conformation password"})
+        else:
+            return render(request,"authentication/signup.html",{'username_error_message': "Username Already Exist"})
+          
     return render(request, "authentication/signup.html")
 
 
 def signin(request):
-
     if request.method == "POST":
         username = request.POST['username']
         pass1 = request.POST['pass1']
-
         user = authenticate(username=username, password=pass1)
 
         if user is not None:
             login(request, user)
             return render(request, "authentication/dashboard.html")
         else:
-            messages.error(request, "Bad Credentials!")
-            return redirect('home')
+            return render(request,"authentication/signin.html",{'error_message': "Check your Credentials"})
 
     return render(request, "authentication/signin.html")
 
